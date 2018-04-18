41168fe82f64a7b03c61ea943b33a26bb14839b1
Emanuel-Strom <Emanuel.strom@iggot.se>
2018-04-18 13:46:55 +0200

    min of three

---
+++ lib/min_of_three.rb
@@ -0,0 +1,14 @@
+def min_of_three(num1, num2, num3)
+
+    if num1 > num2
+        min = num2
+    else
+        min = num1
+    end
+
+    if min > num3
+        min = num3        
+    end
+
+return min
+end
