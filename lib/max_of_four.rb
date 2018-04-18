f8006758a7be19ebf76528f0ecd63fb658e22e14
Emanuel-Strom <Emanuel.strom@iggot.se>
2018-04-18 14:05:28 +0200

    max of four

---
+++ lib/max_of_four.rb
@@ -0,0 +1,16 @@
+def max_of_four(num1, num2, num3, num4)
+    max = num1
+    if num2 > max
+        max = num2
+    end
+
+    if num3 > max
+        max = num3
+    end
+
+    if num4 > max
+        max = num4
+    end
+
+return max
+end
